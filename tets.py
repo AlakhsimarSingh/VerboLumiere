bd6a773f8c2bb8ad4a0bb54283f4c69b6791745f
AlakhsimarSingh <alakh124@gmail.com>
2025-05-18 16:25:29 +0530

    re-configured and solved the remaining errors

---
+++ tets.py
@@ -0,0 +1,8 @@
+import torch
+print("CUDA available:", torch.cuda.is_available())
+
+import moviepy.editor
+print("moviepy imported successfully")
+
+import transformers
+print("transformers imported successfully")
